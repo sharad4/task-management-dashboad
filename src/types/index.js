a9c5b0421410786a888edade5b3bb2d1e9c0b5ec
sharad4 <sharad011989@gmail.com>
2025-07-22 01:40:11 +0530

    add types.js

---
+++ src/types/index.js
@@ -0,0 +1,17 @@
+export const TaskStatus = {
+    TODO: 'todo',
+    IN_PROGRESS: 'in_progress',
+    COMPLETED: 'completed'
+};
+
+export const TaskPriority = {
+    LOW: 'low',
+    MEDIUM: 'medium',
+    HIGH: 'high'
+};
+
+export const SortBy = {
+    CREAETD_AT: 'createdAt',
+    PRIORITY: 'priority',
+    TITLE: 'title'
+};

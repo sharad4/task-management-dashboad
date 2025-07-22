e10b92365f656bd7b9e6232651434fcd6c7d8ca6
sharad4 <sharad011989@gmail.com>
2025-07-22 14:01:00 +0530

    Auto-commit: update CustomInput.jsx

--- src/components/ui/CustomInput.jsx
+++ src/components/ui/CustomInput.jsx
@@ -20,7 +20,10 @@ const CustomInput = forwardRef(
         <input
           id={id}
           ref={ref}
-          className={`w-full p-3 border rounded-lg focus-within:`}
+          className={`w-full p-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors ${
+            error ? "border-red-500" : "border-gray-300"
+          } ${className}`}
+          {...props}
         />
       </div>
     );

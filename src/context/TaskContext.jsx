6a52dacbe74f03911648c361cf8d9b5883accf13
sharad4 <sharad011989@gmail.com>
2025-07-22 12:17:23 +0530

    Auto-commit: update TaskContext.jsx

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -5,3 +5,13 @@ const TaskContext = createContext();
 export const TaskProvider = ({ children, value }) => {
   return <TaskContext.Provider value={value}>{children}</TaskContext.Provider>;
 };
+
+export const useTaskContext = () => {
+  const context = useContext(TaskContext);
+  if (!context) {
+    throw new Error(`useTaskContext must be used within TaskProvider`);
+  }
+  return context;
+};
+
+export default TaskContext;

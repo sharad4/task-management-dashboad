609bfb67ce389c1c521f250d878f09d117d9bf83
sharad4 <sharad011989@gmail.com>
2025-07-22 12:05:49 +0530

    Auto-commit: update ThemeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,3 +1,17 @@
 import { createContext, useContext, useState } from "react";
 
 const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const value = {
+    theme,
+    setTheme,
+    isDark: theme === "dark",
+  };
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+};
